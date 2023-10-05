e54fbd6daaec8077b46533b41861a54f69920d6d
believerabhi <abhi57m@gmail.com>
2023-10-06 02:14:35 +0530

    RememberSaveable in compose

--- app/src/main/java/com/example/jetpack/compose/state/ui/screen/WellnessScreen.kt
+++ app/src/main/java/com/example/jetpack/compose/state/ui/screen/WellnessScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -18,7 +19,7 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = Modifier.padding(16.dp)) {
-        var count by  remember { mutableStateOf(0) }
+        var count by  rememberSaveable { mutableStateOf(0) }
 
         if(count>0) {
             var showTask by remember { mutableStateOf(true) }
